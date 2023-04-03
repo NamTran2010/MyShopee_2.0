06f5f033c8156649aaba7bfae6181527e8ec00d1
NamTran2010 <110371223+NamTran2010@users.noreply.github.com>
2023-04-03 13:47:37 +0700

    add project

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
